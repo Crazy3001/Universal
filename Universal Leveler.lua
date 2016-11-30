a2d734bea42de6ccec44fc2527aaadf7022643d4
Crazy3001 <roy.nathan89@gmail.com>
2016-11-30 17:18:53 -0400

    Add files via upload

--- Universal Leveler.lua
+++ Universal Leveler.lua
@@ -5,17 +5,29 @@ author = "Crazy3001"
 				-------------------CONFIGURATION-------------------
 				--#################################################--
 --Favorites:
---Seafoam B4F 
+--Seafoam B4F (56,14,61,14)
 --Dragons Den
 --Victory Road Kanto 3F
---
---
 
 
-				--#################################################--
-				-------------------GLOBAL SETTINGS-------------------
-				--#################################################--
-				
+--Case Sensitive--
+--Put the name of the map you want to train at between "". {Example: location = "Route 121"}
+local location = ""		
+
+-- Put "Grass" for grass, "Water" for water, {x, y} for fishing cell, {x1, y1, x2, y2} for rectangle
+-- If you're using a rectangle, you can set more rectangles to hunt in just by adding 4 more parameters. Example: local area = {x1, y1, x2, y2, x1, y1, x2, y2}
+local area = ""
+		
+-- If you're using multiple rectangles, this is the amount of time in minutes that we'll stay in one rectangle before moving to a different one
+local minutesToMove = 30
+
+--Will catch any Pokemon that is not registered as owned in your Pokedex.
+local catchNotCaught = false
+
+--the below is case-sensitive, add more moves by adding commas. example : catchThesePokemon = {"Pokemon 1", "Pokemon 2", "Pokemon 3"}--
+--Even if you set all other capture variables to false, we'll still try to catch these/this pokemon--
+--Leave an empty "" here if you aren't using it--
+local catchThesePokemon = {""}
 
 --When leveling, if there are any Pokemon you do not want to fight, put them in here. Example : evadeThesePokemon = {"Pokemon 1", "Pokemon 2", "Pokemon 3"}--
 --Leave an empty "" here if you aren't using it--
@@ -27,14 +39,11 @@ local levelPokesTo = 100
 --What level you want your pokemon to start fight instead of switching out.
 local minLevel = 1
 
--- The Bot will heal your Pokemon when the Count of alive Pokemon get to this number
-local keepAlive = 0
-
---This is the percentage of your last alive usable poke's health that we'll stop fighting at and go to pokecenter--
-local healthToRunAt = 30 
-
 --If set true, if you have Leftovers, it will automatically give it to your first usable pokemon.--
-local useLeftovers = true
+local useLeftovers = false
+
+--The percentage of your last alive usable poke's health that we'll stop fighting at and go to pokecenter--
+local healthToRunAt = 30 
 
 --Type of rod you want to use when fishing
 local typeRod = "Super Rod"
@@ -42,58 +51,6 @@ local typeRod = "Super Rod"
 --the below is case-sensitive, add more moves by adding commas. ex : movesNotToForget = {"Move 1", "Move 2", "Move 3"}--
 --Leave an empty "" here if you aren't using it--
 local movesNotToForget = {"Dig", "Cut", "Surf", "Flash", "Rock Smash", "Dive"}
-
-
-				--#################################################--
-				-------------------LOCATION SETTINGS-------------------
-				--#################################################--
-
-				
---Case Sensitive--
---Put the name of the map you want to train at between "". {Example: location = "Route 121"}
-local location = ""
-
--- Put "Grass" for grass, "Water" for water, {x, y} for fishing cell, {x1, y1, x2, y2} for rectangle
--- If you're using a rectangle, you can set more rectangles to hunt in just by adding 4 more parameters. Example: local area = {x1, y1, x2, y2, x1, y1, x2, y2}
-local area = "Water"
-		
--- If you're using multiple rectangles, this is the amount of time in minutes that we'll stay in one rectangle before moving to a different one
-local minutesToMoveArea = 30
-
-
-				--#################################################--
-				-------------------CATCH SETTINGS-------------------
-				--#################################################--
-				
-				
---Will catch any Pokemon that is not registered as owned in your Pokedex.
-local catchNotCaught = false
-
---the below is case-sensitive, add more moves by adding commas. example : catchThesePokemon = {"Pokemon 1", "Pokemon 2", "Pokemon 3"}--
---Even if you set all other capture variables to false, we'll still try to catch these/this pokemon--
---Leave an empty "" here if you aren't using it--
-local catchThesePokemon = {""}
-
---If using False Swipe for catching, set true.
-local useSwipe = false
-
---Determines the percentage that the opponents health has to be to start throwing pokeballs. If using False Swipe, leave at 1.
-local throwHealth = 10
-				
-
-				--#################################################--
-				-------------------BALL SETTINGS-------------------
-				--#################################################--
-				
-				
---Must be filled in. Determines what type of ball to use when catching, and what type to buy. Example: typeBall = "Pokeball"
-local typeBall = "Pokeball"
---Set true if you want to buy your type of ball when you get low.
-local buyBalls = false
---If buying balls, put in the amount of balls you want to have in your inventory.
-local buyBallAmount = 500
---Will buy more balls when your type of ball reaches X.
-local buyBallsAt = 100
 		
 
 				--#################################################--
@@ -106,7 +63,7 @@ local buyBallsAt = 100
 				--#################################################--
 
 				
-description = "Leveling pokemon to level " .. levelPokesTo .. ". Press Start"
+description = "Training at " .. location .. "." .. " Leveling pokemon to level " .. levelPokesTo .. ". Press Start"
 
 local pf = require "PathFinder/MoveToApp"
 local Lib = require "PathFinder/Lib/lib"
@@ -121,7 +78,6 @@ function onStart()
     tmpRand = 0 -- Used to make sure rand is different every time we call math.random
     lineSwitch = false -- Used in moveToLine()
     rectTimer = os.time()
-	locTimer = os.time()
     pokemonTable = {}
     for i = 1, getTeamSize() do
         pokemonTable[i] = {}
@@ -130,6 +86,10 @@ function onStart()
         pokemonTable[i][2] = getPokemonLevel(i)
     end
     startMoney = getMoney()
+    for i = 1, #pokemonTable do
+        log("Your " .. pokemonTable[i][0] .. " is Level " .. getLevelFromId(pokemonTable[i][1]) .. ".")
+    end
+    log("You have currently " .. getMoney() .. " Pokedollars.")
 end
 
 function onPause()
@@ -172,10 +132,14 @@ function onBattleMessage(wild)
 	    log("********************************************************************************************")
 		log("*********************************** SESSION STATS ***************************************")
 		log("********************************************************************************************")
+		for i = 1, #pokemonTable do
+			log("Your level " .. getLevelFromId(pokemonTable[i][1]) .. " " .. pokemonTable[i][0] .. " gained " .. getLevelFromId(pokemonTable[i][1]) - pokemonTable[i][2] .. " levels!")
+		end
 		log("Shinies encountered: " .. shinyCounter)
 		log("Pokemon encountered: " .. wildCounter)
 		log("Pokemon caught: " .. catchCounter)
 		log("You have visited the PokeCenter ".. healCounter .." times.")
+		log("You have earned ".. tostring(getMoney() - startMoney) .." PokeDollars!")
 		log("*********************************************************************************************")
     elseif stringContains(wild, "You failed to run away") then
         failedRun = true
@@ -186,6 +150,14 @@ end
 
 function onLearningMove(moveName, pokemonIndex)
    forgetAnyMoveExcept(movesNotToForget)
+end
+
+function getLevelFromId(ID)    
+    for i = 1, getTeamSize() do
+        if getPokemonUniqueId(i) == ID then
+            return getPokemonLevel(i)
+        end
+    end    
 end
 
 function TableLength(T)
@@ -281,8 +253,8 @@ function updateFishing(list)
 end
 
 function updateTargetRect(list)
-	-- Every minutesToMoveArea minutes, picks a random integer between 1 and #list / 4 to get a number corresponding to each rectangle in list	
-	if os.difftime(os.time(), rectTimer) > minutesToMoveArea * 60 or rand == 0 or rand > #list / 4 or rand == tmpRand then
+	-- Every minutesToMove minutes, picks a random integer between 1 and #list / 4 to get a number corresponding to each rectangle in list	
+	if os.difftime(os.time(), rectTimer) > minutesToMove * 60 or rand == 0 or rand > #list / 4 or rand == tmpRand then
 		rectTimer = os.time()
 		tmpRand = rand
 		rand = math.random(#list / 4)
@@ -296,15 +268,6 @@ function updateTargetRect(list)
 		return moveToRectangle(list[n + 1], list[n + 2], list[n + 3], list[n + 4])
 	end
 end
-
---[[function updateLocation(list)
-local map = getMapName()
-	if os.difftime(os.time(), locTimer) > minutesToMoveLocation * 60 then
-		locTimer = os.time()
-	end
-	
-	return pf.moveTo(map, list[math.random(#list)])
-end]]
 
 function moveToLine(list)	
 	-- Alternates between 2 positions	
@@ -323,79 +286,14 @@ function moveToLine(list)
 	end
 end
 
-function getLevelFromId(ID)    
-    for i = 1, getTeamSize() do
-        if getPokemonUniqueId(i) == ID then
-            return getPokemonLevel(i)
-        end
-    end    
-end
-
-function hasUsablePokemonWithMove(Move)
-	local hasUsablePokemonWithMove = {}
-	hasUsablePokemonWithMove["id"] = 0
-	hasUsablePokemonWithMove["move"] = nil
-	local statusMoveList = {"glare", "stun spore", "thunder wave", "hypnosis", "lovely kiss", "sing", "sleep spore", "spore"}
-	if Move == statusMove then
-		for _,Move in pairs(statusMoveList) do
-			for i=1, getTeamSize(), 1 do
-				if hasMove(i, Move) and getRemainingPowerPoints(i, Move) >= 1 and isPokemonUsable(i) then
-					hasUsablePokemonWithMove["id"] = i
-					hasUsablePokemonWithMove["move"] = Move
-					return hasUsablePokemonWithMove, true
-				end
-			end
-		end
-	else
-		for i=1, getTeamSize(), 1 do
-			if hasMove(i, Move) and getRemainingPowerPoints(i, Move) >= 1 and isPokemonUsable(i) then
-				return i, true
-			end
-		end		
-	end
-	return false
-end
-
-function hasPokemonWithMove(Move)
-	local hasPokemonWithMove = {}
-	hasPokemonWithMove["id"] = 0
-	hasPokemonWithMove["move"] = nil
-	local statusMoveList = {"glare", "stun spore", "thunder wave", "hypnosis", "lovely kiss", "sing", "sleep spore", "spore"}
-	if Move == statusMove then
-		for _,Move in pairs(statusMoveList) do
-			for i=1, getTeamSize(), 1 do
-				if hasMove(i, Move) then
-					hasPokemonWithMove["id"] = i
-					hasPokemonWithMove["move"] = Move
-					return hasPokemonWithMove, true
-				end
-			end
-		end
-	else
-		for i=1, getTeamSize(), 1 do
-			if hasMove(i, Move) then
-				return i, true
-			end
-		end		
-	end
-	return false
-end
-
 function hasRemainingPP(i)
-local powerCount = 0
-local ppCount = 0
 	for m=1, 4, 1 do
 		if getPokemonMovePower(i,m) >= 1 then
-			powerCount = powerCount + 1
 			p = getPokemonMoveName(i,m)
 			if getRemainingPowerPoints(i,p) >= 1 then
-				ppCount = ppCount + 1
+				return true
 			end
 		end
-	end
-	
-	if ppCount >= 1 then
-		return true
 	end
 	Lib.log1time("######No More PP, Using Pokecenter######")
 	return false
@@ -480,50 +378,14 @@ function allPokemonReachedTargetLevel()
 	return false
 end
 
-function getTotalAlivePokemonCount()
-	local count = 0
-	for i=1, getTeamSize(), 1 do
-		if getPokemonHealth(i) >= 1 then
-			count = count + 1
-		end
-	end
-	if count == keepAlive then
-		Lib.log1time("######keepAlive Number Reached, Using Pokecenter######")
-	end
-	return count
-end
-
-function goToArea()
-	if type(area) == "string" then
-		area = area:upper()
-	else
-		if #area == 2 then
-			return updateFishing(area)
-		elseif #area > 4 then
-			return updateTargetRect(area)
-		elseif area[1] == area[3] or area[2] == area[4] then
-			return moveToLine(area)
-		else
-			return moveToRectangle(area[1], area[2], area[3], area[4])
-		end
-	end
-	
-	if area == "GRASS" then
-		return moveToGrass()
-	elseif area == "WATER" then
-		return moveToWater()
-	end
-end
-
 function onPathAction()
 local map = getMapName()
+local location = location
 canNotSwitch = false
 failedRun = false
 
-	if buyBalls then
-		if getItemQuantity(typeBall) <= buyBallsAt then
-			return pf.useNearestPokemart(map, typeBall, ballAmount)
-		end
+	if getMapName() == "Prof. Antibans Classroom" then
+		logout()
 	end
 
 	if isSorted() then 
@@ -537,12 +399,29 @@ failedRun = false
 	end
 	
 	if not allPokemonReachedTargetLevel() then
-		if getTotalUsablePokemonCount() >= 1 and getTotalAlivePokemonCount() >= keepAlive and (getTotalPokemonToLevelCount() > 1 or (getTotalPokemonToLevelCount() == 1 and getPokemonHealthPercent(getFirstPokemonToLevel()) >= healthToRunAt)) then
-			if map == location then
-				goToArea()
-			else
-				pf.moveTo(map, location)
-			end			
+		if getTotalUsablePokemonCount() >= 1 and (getTotalPokemonToLevelCount() > 1 or (getTotalPokemonToLevelCount() == 1 and getPokemonHealthPercent(getFirstPokemonToLevel()) >= healthToRunAt)) then
+			if getMapName() == location then
+				if type(area) == "string" then
+					location = area:upper()
+				else
+					if #area == 2 then
+						return updateFishing(area)
+					elseif #area > 4 then
+						return updateTargetRect(area)
+					elseif area[1] == area[3] or area[2] == area[4] then
+						return moveToLine(area)
+					else
+						return moveToRectangle(area[1], area[2], area[3], area[4])
+					end
+				end
+				
+				if location == "GRASS" then
+					return moveToGrass()
+				elseif location == "WATER" then
+					return moveToWater()
+				end
+			else pf.moveTo(map, location)
+			end
 		else
 			pf.useNearestPokecenter(map)
 		end
@@ -554,34 +433,15 @@ end
 function onBattleAction()
 	if isWildBattle() and isOpponentShiny() or isOnList(catchThesePokemon) or (catchNotCaught and not isAlreadyCaught()) then
 		if isPokemonUsable(getActivePokemonNumber()) then
-			if getOpponentHealthPercent() > throwHealth then	
-				if useSwipe and hasUsablePokemonWithMove("False Swipe") then
-					if getActivePokemonNumber() == hasUsablePokemonWithMove("False Swipe") then
-						if useMove("False Swipe") then return end
-					else
-						if sendPokemon(hasUsablePokemonWithMove("False Swipe")) then return end
-					end
-				else
-					if isPokemonUsable(getActivePokemonNumber()) then
-						if weakAttack() then return end
-					else
-						if sendUsablePokemon() or sendAnyPokemon() or run() then return end
-					end
-				end
-
-			else
-				if isPokemonUsable(getActivePokemonNumber()) then
-					if useItem(typeBall) then return end
-				else
-					if sendUsablePokemon() or sendAnyPokemon() or run() then return end
-				end
-			end	
+			if useItem("Ultra Ball") or useItem("Great Ball") or useItem("Pokeball") then
+				return
+			end
 		else
 			return sendUsablePokemon() or sendAnyPokemon()
 		end
 	end
 	if isWildBattle() and not isOnList(evadeThesePokemon) then
-		if getTotalUsablePokemonCount() < 1 or (getTotalPokemonToLevelCount() < 1 or (getTotalPokemonToLevelCount() == 1 and getPokemonHealthPercent(getFirstPokemonToLevel()) < healthToRunAt)) or getTotalAlivePokemonCount() == keepAlive then
+		if getTotalUsablePokemonCount() < 1 or (getTotalPokemonToLevelCount() < 1 or (getTotalPokemonToLevelCount() == 1 and getPokemonHealthPercent(getFirstPokemonToLevel()) < healthToRunAt)) then
 			if isPokemonUsable(getActivePokemonNumber()) then
 				return run()
 			else
